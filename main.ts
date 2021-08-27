221b56b1d9ff767962fdf2416351f70a900d1391
dExter205 <88722109+dExter205@users.noreply.github.com>
2021-08-27 09:02:23 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    led.plot(0, 1)
+    led.plot(2, 0)
+    led.plot(0, 3)
+    led.plot(0, 0)
+    led.plot(4, 3)
+    led.plot(4, 4)
+    led.plot(0, 4)
+    led.plot(4, 0)
+    led.plot(4, 1)
+    led.plot(4, 1)
+    led.plot(0, 0)
+    led.plot(0, 0)
+})
+basic.forever(function () {
+	
+})
